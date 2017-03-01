793e813591dd32b8a8d4cf341efdf95e71b3a876
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2017-03-01 14:25:33 +0300

    Init the repo

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. popsim documentation master file, created by
+   sphinx-quickstart on Wed Mar  1 14:17:19 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to popsim's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license_ = f.read()
+
+setup(
+    name='popsim',
+    version='0.1.0',
+    description='A tool to simulate progeny from a plink ped file',
+    long_description=readme,
+    author='Ivan Dmitrievsky',
+    author_email='ivan.dmitrievsky+opensource@gmail.com',
+    url='https://gitlab.com/idmit/popsim',
+    install_requires=[
+
+    ],
+    license=license_,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
